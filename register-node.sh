8dc92c6045eb35459d49a5a0e86de7562e5c6483
shirahcashriel <shirah.cashriel@edence.health>
2026-01-02 10:27:52 +0100

    $HOME/.local/bin

--- register-node.sh
+++ register-node.sh
@@ -11,6 +11,7 @@ fi
 if ! command -v uv &> /dev/null; then
     echo "Installing uv..."
     curl -LsSf https://astral.sh/uv/install.sh | sh
+    source $HOME/.local/bin/env
     source $HOME/.cargo/env
 fi
 
